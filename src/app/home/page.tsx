3e50d6aa1f525f323e3d62ac756d53f323c5c539
hjnengare <hjnengare@gmail.com>
2025-11-29 11:03:02 +0200

    Update home page spacing and layout

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -203,7 +203,7 @@ export default function Home() {
       />
       
       <main className="bg-off-white relative pt-20 sm:pt-24 pb-28">
-        <div className="mx-auto w-full max-w-[2000px] px-2">
+        <div className="mx-auto w-full max-w-[2000px]">
           {/* Search Input at top of home content */}
           <div ref={searchWrapRef} className="py-4 px-4">
             <SearchInput
